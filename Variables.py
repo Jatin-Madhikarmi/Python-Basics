61a3c892f1d792b5b77ebff9a697b95a18f897bf
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2025-12-29 22:42:22 +0545

    Normalizing line endings using .gitattributes and making some changes to the Variables.py file.

--- Variables.py
+++ Variables.py
@@ -2,3 +2,21 @@ a=5
 y="hello"
 
 print(y)
+print(type(y))
+print(len(y))
+print(y[0:-1])
+print(y[0:1])
+print(y[::-1])
+print(y[::-2])
+str_a="I love Python"
+str_b=" very much" 
+print(str_a + str_b)
+num=1
+print("num=" + str(num))
+
+str_1="KU"
+str_2="Nepal"
+print(f"{str_1} is a good uni in {str_2}")
+
+print ("{3*4}")
+print (f"{3*4}") # here the f stands for the f-string method
