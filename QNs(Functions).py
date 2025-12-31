4d0575a17f16414e4ada008cb51f52e07544fa5c
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2026-01-01 00:29:39 +0545

    Solved the basic conditon for the matrix slicing

--- QNs(Functions).py
+++ QNs(Functions).py
@@ -16,9 +16,24 @@ print(f"The area of the traingle where breadth is {b} and height is {h} is {trai
 
 
 def splitMatrix(m):
-    print(m.shape)
+    print(f"The size dimension(shape) of the matrix is {m.shape}")
+    print(type(m.shape))
+    list1=list(m.shape)
+    print(list1[1]) # represents the column of the matrix to determine whether it is odd or even
+    if list1[1]/2 != 0:
+        sliceVar1=list1[1]-2
+        sliceVar2=2
+    else:
+        sliceVar1=list1[1]/2    
+        sliceVar2=list1[1]/2   
+    m1=m[:,[0,sliceVar1]]
+    m2=m[:,[sliceVar2]]
+    return [m1,m2]
 
 
 m=np.array([[1,2,3],[4,5,6],[7,8,9]])
+#m=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
 print(m)
-splitMatrix(m)
+list1=splitMatrix(m)
+print(list1[0])
+print(list1[1])
