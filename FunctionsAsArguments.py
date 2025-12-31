4f2fdee693cc7d889ef41d7ae78be3076f5b8191
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2025-12-31 23:24:14 +0545

    Further understanding of functions as arguments

--- FunctionsAsArguments.py
+++ FunctionsAsArguments.py
@@ -9,7 +9,11 @@ print(max([1,2,5]))
 def func_plus_1(f,x):
     return (f(x) + 1)
 
+
+x,y=5,2
 print(func_plus_1(np.sin,np.pi/2))
 print(func_plus_1(np.cos,np.pi/2))
 print(func_plus_1(np.sin,np.pi))
 print(func_plus_1(np.cos,np.pi))
+print(func_plus_1(lambda x:x+2,2))
+print(func_plus_1(lambda x:x**2,3))
