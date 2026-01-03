c7bb76c71cbd896a28a8809b598b826d19f7cc08
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2026-01-03 22:58:37 +0545

    Implemented for loops

---
+++ Loops.py
@@ -0,0 +1,62 @@
+import numpy as np
+n=0
+for i in range(1,4):
+    n=n+i
+
+print(n)
+
+for c in "watermelon":
+    print(c)
+
+s="apple"
+for i in range(len(s)):
+    print(s[i])
+
+x=0
+y=[2,3,1,3,3]
+
+for i in y:
+    x+=i
+
+print(x)
+
+dict1={"One":1,
+       "Two":2,
+       "Three":3}
+
+for key in dict1.keys():
+    print(key,dict1[key])
+
+for key,value in dict1.items():
+    print(key,value)
+
+a=["One","Two","Three"]
+b=[1,2,3]
+
+for i,j in zip(a,b):
+    print(i,j)
+
+def haveDigits(s):
+    out=0
+
+    for c in s:
+        if c.isdigit():
+            out=1
+            break;
+    return out
+
+print(haveDigits("Only for you"))
+
+for i in range(5):
+    if i==2:
+        continue
+    print(i)
+
+x=np.array([[1,2],[3,4]])
+m,n=x.shape
+s=0
+for i in range(n):
+    for j in range(m):
+        s+=x[i,j]
+
+print(s)
