5a7b2f052c0b0646c828ddc6e61e569a1a62fec2
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2026-01-08 23:01:19 +0545

    Solved some questions realted to recusrsions

--- QNs(Recursion).py
+++ QNs(Recursion).py
@@ -9,15 +9,19 @@ list1=[1,12,3,22,1,1]
 print(my_sum_list(list1))
 print(my_sum_list(range(1,101)))
 
-def my_chebyshev_poly1(n,x):
-    if n == 0:
-        return [1]*x.lenght()
-    
-    elif n == 1:
-        return x
-    
-    else:
-        return 2*my_chebyshev_poly1()
+def my_chebyshev_poly1(n, x):
+    def get_tn(n,val):
+        if n == 0:
+            return 1
+        elif n== 1:
+            return x
+        else:
+            return 2 * val * get_tn(n - 1, val) - get_tn(n - 2, val)
+        
+
+    y=[get_tn(n,val) for val in x]
+    return y
+
 x=[0,1,2,3,4,5]
 n=0
 print(my_chebyshev_poly1(n,x))
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-for i in M:
-    counter+=1
-    if i == 1:
-        y.append(counter)
