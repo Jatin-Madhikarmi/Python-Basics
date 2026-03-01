8eb8e9b9377b2dff011fac4c365e0b8bfaed0007
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2026-03-01 19:48:54 +0545

    Also implemented a simple delta decompression

--- DeltaCompression.py
+++ DeltaCompression.py
@@ -9,8 +9,20 @@ def delta_compression(uncompressed_list):
 
     return compressed_list
 
+def delta_decompresseion(compressed_list):
+    uncompressed_list=[]
+    curr_value=compressed_list[0]
+    prev_value=0
+    for i in compressed_list:
+        curr_value=i+prev_value
+        uncompressed_list.append(curr_value)
+        prev_value=curr_value
+
+    return uncompressed_list
+
 print(f"The uncompressed list is {uncompressed_list}")
 compressed_list=delta_compression(uncompressed_list)
 print(f"The compressed list is {compressed_list}")
-        
+new_uncompressed_list=delta_decompresseion(compressed_list)
+print(f"The new uncompressed list is {new_uncompressed_list}")
 
