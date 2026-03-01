fd3eccfd463043572a4e53482d0253e0142829e6
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2026-03-01 23:25:50 +0545

    Futher improved the command line utility

--- DeltaCompression.py
+++ DeltaCompression.py
@@ -2,42 +2,49 @@ import argparse
 uncompressed_list=[2,3,4,5,2,1,3,5,7]
 
 def delta_compression(uncompressed_list):
+    uncompressed_list=[int(x) for x in uncompressed_list]
     compressed_list=[]
     prev_value=0
     for i in uncompressed_list:
         compressed_list.append(i-prev_value)
         prev_value=i
 
-    return compressed_list
+    print(f"The compressd list is {compressed_list}")
 
-def delta_decompresseion(compressed_list):
+def delta_decompression(compressed_list):
+    compressed_list=[int(x) for x in compressed_list]
     uncompressed_list=[]
-    curr_value=compressed_list[0]
     prev_value=0
     for i in compressed_list:
         curr_value=i+prev_value
         uncompressed_list.append(curr_value)
         prev_value=curr_value
 
-    return uncompressed_list
+    print(f"The uncompressed list is {uncompressed_list}")
 
 def get_parser():
     argparser=argparse.ArgumentParser(description="Command Line Utility to demonstrate the delta compression")
-    argparser.add_argument("list",
-                           nargs='+',
-                           help="Add the list to be compressed")
+    argsubparser=argparser.add_subparsers(title="Commands",dest="command",required=True)
+    args=argsubparser.add_parser("compression",
+                                help="To do the delta compression")
+    args.add_argument("compression",
+                      nargs="+",
+                      help="Provide the list")
+    args=argsubparser.add_parser("decompression",
+                                 help="To do the delta decompression")
+    args.add_argument("decompression",
+                      nargs="+",
+                      help="Provide the list")
     
     return argparser
 
 def main(argv=None):
     parser=get_parser()
     args=parser.parse_args(argv)
-    uncompressed_list=[int(x) for x in args.list]
-    print(f"The uncompressed list is {uncompressed_list}")
-    compressed_list=delta_compression(uncompressed_list)
-    print(f"The compressed list is {compressed_list}")
-    # new_uncompressed_list=delta_decompresseion(compressed_list)
-    # print(f"The new uncompressed list is {new_uncompressed_list}")
+    match args.command:
+        case "compression":delta_compression(args.compression)
+        case "decompression":delta_decompression(args.decompression)
+        case _:raise Exception(f"INVALID COMMAND {args.command}")
 
 if __name__=="__main__":
     main()
