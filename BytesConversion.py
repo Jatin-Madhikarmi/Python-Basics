1ae9d4f3e61965d4e5de26da9adb4206e4a35ff4
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2026-03-06 11:16:30 +0545

    Revisited Bytes

--- BytesConversion.py
+++ BytesConversion.py
@@ -8,3 +8,12 @@ print(f"The int number is {int_number}")
 number=12
 byte_number=format(number,'b')
 print(byte_number)
+
+sha='04c3f44424e60ba98e3a3f13d3d70ccab9b23592'
+int_sha=int(sha,16)
+byte_sha=int_sha.to_bytes(20,"big")
+from_byte_to_int_sha=int.from_bytes(byte_sha,"big")
+print(sha)
+print(int_sha)
+print(byte_sha)
+print(from_byte_to_int_sha)
